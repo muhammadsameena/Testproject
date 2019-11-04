f03cd2b0faaf140226d1585a456f77bb263fd2b3
Mohammed Sameena <sameenamohd98@gmail.com>
2019-11-04 16:30:51 +0530

    files

---
+++ Testproject/src/files/ReadHello.java
@@ -0,0 +1,18 @@
+package files;
+import java.io.*;
+public class ReadHello {	 
+	public static void main(String args[]){
+	try{
+	FileInputStream fin=new FileInputStream("abc.txt");
+	int i=0;
+	while((i=fin.read())!=-1){
+	System.out.println((char)i);
+	}
+	fin.close();
+	} catch(Exception e){
+	System.out.println(e);}
+	}
+	
+	}
+
+
